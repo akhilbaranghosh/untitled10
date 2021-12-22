e97df7fd98f52ee49f47d8ce42372c1d9d7082c5
akhil <akhilbaran.ghosh@aot.edu.in>
2021-12-22 16:06:40 +0530

    first entry

---
+++ src/main.erl
@@ -0,0 +1,40 @@
+%%%-------------------------------------------------------------------
+%%% @author akhil
+%%% @copyright (C) 2021, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20. Dec 2021 15:49
+%%%-------------------------------------------------------------------
+-module(main).
+-author("akhil").
+%% API
+-export([start/1,average/1,sending_numbers/2,print_numbers/0]).
+%%factorial(0,Acc) -> Acc;
+%%factorial(N,Acc) -> factorial(N-1,Acc*N).
+
+start(X)->
+  io:fwrite("the value of 10 ~p :",[X]).
+
+average(L)->
+  receive
+    N ->
+      Avg=(lists:nth(1,L)+N)/2,
+      io:fwrite("cuerrent average: ~p~n",[Avg]),
+      average([Avg|L])
+  end.
+print_numbers()->
+  receive
+    {Pid,N} ->
+      io:fwrite("the number sent is:~p~n",[N]),
+      Pid!{self(),print_successful}
+  end.
+
+sending_numbers(1,Pid)->Pid!1;
+sending_numbers(N,Pid)->
+  Pid!{self(),N},
+  receive
+    {Pid,print_successful} ->
+      sending_numbers(N-1,Pid)
+  end.
+
